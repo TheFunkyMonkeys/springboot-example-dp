c9f65e775c31a124372efb181d7818b9580665e3
공대표 <c5ngdp@gmail.com>
2023-04-25 15:12:38 +0900

    Add springboot initialized

---
+++ src/main/java/com/springbootexample/springbootexampledp/SpringbootExampleDpApplication.java
@@ -0,0 +1,13 @@
+package com.springbootexample.springbootexampledp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExampleDpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExampleDpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootexample/springbootexampledp/SpringbootExampleDpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootexample.springbootexampledp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExampleDpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
