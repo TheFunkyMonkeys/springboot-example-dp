eee6ed3968def1cc3f943d5b0868556dc12368d4
공대표 <c5ngdp@gmail.com>
2023-05-02 19:55:54 +0900

    init

--- src/main/java/com/springbootexample/springbootexampledp/SpringbootExampleDpApplication.java
+++ src/main/java/com/springbootexample/springbootexampledp/SpringbootExampleDpApplication.java
@@ -8,7 +8,6 @@ public class SpringbootExampleDpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootExampleDpApplication.class, args);
-		System.out.println("Test");
 	}
 
 }
